ca24d1458f929f3f14f1028bbe23785c5a4e38a7
timickb <timikm2002@gmail.com>
2021-01-30 00:55:04 +0300

    Settings Form

--- NotepadPlus/FormatContextMenu.cs
+++ NotepadPlus/FormatContextMenu.cs
@@ -44,7 +44,7 @@ namespace NotepadPlus
             Items.Add(_fontChangeMenuItem);
             Items.Add(_colorChangeMenuItem);
             
-            if (s.Type == FileType.RichText)
+            if (s.Type == FileType.PlainText)
             {
                 _fontChangeMenuItem.Enabled = false;
                 _colorChangeMenuItem.Enabled = false;
@@ -66,14 +66,44 @@ namespace NotepadPlus
         /// <returns></returns>
         public ToolStripItem[] GetRangeOfItems()
         {
+            // KOSTYL: пришлось создавать копии пунктов меню (дублирование
+            // огромного куска кода из конструктора выше). Оправадание:
+            // торопился к дедлайну,а передача их по ссылке не работала правильно.
+            ToolStripMenuItem copyMenuItem = new ToolStripMenuItem(_copyMenuItem.Text);
+            ToolStripMenuItem pasteMenuItem = new ToolStripMenuItem(_pasteMenuItem.Text);
+            ToolStripMenuItem selectAllMenuItem = new ToolStripMenuItem(_selectAllMenuItem.Text);
+            ToolStripMenuItem cutMenuItem = new ToolStripMenuItem(_cutMenuItem.Text);
+            ToolStripMenuItem colorChangeMenuItem = new ToolStripMenuItem(_colorChangeMenuItem.Text);
+            ToolStripMenuItem fontChangeMenuItem = new ToolStripMenuItem(_fontChangeMenuItem.Text);
+            
+            copyMenuItem.Click += _session.OnSelectionCopy;
+            pasteMenuItem.Click += _session.OnBufferPaste;
+            selectAllMenuItem.Click += _session.OnSelectedAll;
+            cutMenuItem.Click += _session.OnCutSelection;
+            fontChangeMenuItem.Click += _session.OnFontChange;
+            colorChangeMenuItem.Click += _session.OnForeColorChange;
+            
+            if (_session.Type == FileType.PlainText)
+            {
+                fontChangeMenuItem.Enabled = false;
+                colorChangeMenuItem.Enabled = false;
+            }
+            
+            copyMenuItem.ShortcutKeys = Keys.Control | Keys.C;
+            pasteMenuItem.ShortcutKeys = Keys.Control | Keys.V;
+            cutMenuItem.ShortcutKeys = Keys.Control | Keys.X;
+            selectAllMenuItem.ShortcutKeys = Keys.Control | Keys.A;
+            fontChangeMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.F;
+            colorChangeMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.C;
+            
             return new ToolStripItem[]
             {
-                _copyMenuItem,
-                _pasteMenuItem,
-                _selectAllMenuItem,
-                _cutMenuItem,
-                _fontChangeMenuItem,
-                _colorChangeMenuItem
+                copyMenuItem, 
+                pasteMenuItem,
+                selectAllMenuItem,
+                cutMenuItem,
+                fontChangeMenuItem,
+                colorChangeMenuItem
             };
         }
     }
